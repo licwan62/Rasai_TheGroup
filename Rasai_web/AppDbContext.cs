e1807a50c61a0fe3f9774dd9f8cbe3a5176d9df6
licwan62 <hzwlc6262@gmail.com>
2024-06-13 19:42:39 +1200

    Add Procfile for Heroku deployment

--- Rasai_web/AppDbContext.cs
+++ Rasai_web/AppDbContext.cs
@@ -3,15 +3,13 @@ using Rasai_web.Models;
 
 namespace Rasai_web
 {
-
-
-    public class AppDbContext : DbContext
+    /*public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
 
         public DbSet<Booking> Bookings { get; set; }
-    }
+    }*/
 
 }
---
+++ Rasai_web/Migrations/20240613075838_AddMoreFields.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rasai_web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMoreFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
